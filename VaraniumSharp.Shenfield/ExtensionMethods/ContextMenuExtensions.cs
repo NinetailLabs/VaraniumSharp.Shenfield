ff0b9b10d5c0db2f4b2b6eebd42ed9619559f93a
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2017-12-14 18:49:03 +0200

    feat: SubMenu items. Closes #2
    - Added extension method that allows adding of InputBindings to submenu items

--- VaraniumSharp.Shenfield/ExtensionMethods/ContextMenuExtensions.cs
+++ VaraniumSharp.Shenfield/ExtensionMethods/ContextMenuExtensions.cs
@@ -26,18 +26,23 @@ namespace VaraniumSharp.Shenfield.ExtensionMethods
         /// <param name="modifierKeys">ModifierKeys for triggering the shortcut</param>
         public static void AddInputBinding(this ContextMenu menu, MenuItem menuItem, Action<object, RoutedEventArgs> action, object sender, RoutedEventArgs routedEventArgs, Key key, ModifierKeys modifierKeys)
         {
-            if (modifierKeys == ModifierKeys.None && !key.ToString().Contains("Num"))
-            {
-                throw new ArgumentException("Modifier key cannot be None unless keys from the numpad is used");
-            }
+            menu.InputBindings.Add(GenerateInputBinding(menuItem, action, sender, routedEventArgs, key, modifierKeys));
+        }
 
-            if (menuItem != null)
-            {
-                menuItem.InputGestureText = GenerateGestureString(key, modifierKeys);
-            }
-            var keyGesture = new KeyGesture(key, modifierKeys);
-            var binding = new InputBinding(new RelayCommand(param => action.Invoke(sender, routedEventArgs)), keyGesture);
-            menu.InputBindings.Add(binding);
+        /// <summary>
+        /// Create a shortcut key for an item in the context menu
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown if no modifier key is used and the selected key is not a num pad key</exception>
+        /// <param name="menu">The menu which contains sub-menuitems to which the InputBinding should be added</param>
+        /// <param name="menuItem">MenuItem to set InputGestureText for</param>
+        /// <param name="action">The action that should be executed by the input</param>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="routedEventArgs">Routed event arguments for the event</param>
+        /// <param name="key">The key that triggers the shortcut</param>
+        /// <param name="modifierKeys">ModifierKeys for triggering the shortcut</param>
+        public static void AddInputBinding(this MenuItem menu, MenuItem menuItem, Action<object, RoutedEventArgs> action, object sender, RoutedEventArgs routedEventArgs, Key key, ModifierKeys modifierKeys)
+        {
+            menu.InputBindings.Add(GenerateInputBinding(menuItem, action, sender, routedEventArgs, key, modifierKeys));
         }
 
         #endregion
@@ -82,6 +87,31 @@ namespace VaraniumSharp.Shenfield.ExtensionMethods
             sb.Append(key.ToString());
 
             return sb.ToString();
+        }
+
+        /// <summary>
+        /// Create the InputBinding
+        /// </summary>
+        /// <param name="menuItem"></param>
+        /// <param name="action"></param>
+        /// <param name="sender"></param>
+        /// <param name="routedEventArgs"></param>
+        /// <param name="key"></param>
+        /// <param name="modifierKeys"></param>
+        /// <returns></returns>
+        private static InputBinding GenerateInputBinding(MenuItem menuItem, Action<object, RoutedEventArgs> action, object sender, RoutedEventArgs routedEventArgs, Key key, ModifierKeys modifierKeys)
+        {
+            if (modifierKeys == ModifierKeys.None && !key.ToString().Contains("Num"))
+            {
+                throw new ArgumentException("Modifier key cannot be None unless keys from the numpad is used");
+            }
+
+            if (menuItem != null)
+            {
+                menuItem.InputGestureText = GenerateGestureString(key, modifierKeys);
+            }
+            var keyGesture = new KeyGesture(key, modifierKeys);
+            return new InputBinding(new RelayCommand(param => action.Invoke(sender, routedEventArgs)), keyGesture);
         }
 
         #endregion
