867369cb3d552278868c019f7d4f15cb5925bf9c
Ralph Viktor <ralphviktor@ninetaillabs.com>
2018-03-26 15:58:09 +0200

    feat: StyleLoader. Closes #3
    - Update paket.bootstrapper to latest version
    - Added StyleLoader to load styles from ResourceDictionaries
    - Unit tests

---
+++ VaraniumSharp.Shenfield/HelperClasses/StyleLoader.cs
@@ -0,0 +1,52 @@
+﻿using Serilog;
+using System.Windows;
+using VaraniumSharp.Attributes;
+using VaraniumSharp.Shenfield.Interfaces.HelperClasses;
+
+namespace VaraniumSharp.Shenfield.HelperClasses
+{
+    /// <inheritdoc />
+    [AutomaticContainerRegistration(typeof(IStyleLoader))]
+    public class StyleLoader : IStyleLoader
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public StyleLoader()
+        {
+            _logger = Log.Logger.ForContext("Module", nameof(StyleLoader));
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc />
+        public Style LoadStyle(string styleName)
+        {
+            try
+            {
+                var style = (Style)Application.Current.FindResource(styleName);
+                return style;
+            }
+            catch (ResourceReferenceKeyNotFoundException)
+            {
+                _logger.Verbose("Resource {StyleName} could not be found", styleName);
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>
+        /// Serilog Logger instance
+        /// </summary>
+        private readonly ILogger _logger;
+
+        #endregion
+    }
+}
---
+++ VaraniumSharp.Shenfield/Interfaces/HelperClasses/IStyleLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace VaraniumSharp.Shenfield.Interfaces.HelperClasses
+{
+    /// <summary>
+    /// Assist with loading styles from the <see cref="ResourceDictionary"/>
+    /// </summary>
+    public interface IStyleLoader
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Load a Style from the <see cref="ResourceDictionary"/> that are loaded in the application.
+        /// </summary>
+        /// <param name="styleName">Name of the style to load</param>
+        /// <returns>Style unless it could not be found, in which case null</returns>
+        Style LoadStyle(string styleName);
+
+        #endregion
+    }
+}
