55336720a3a8a14c8e936e658ec1b1a557a4e5aa
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2018-11-26 16:29:37 +0200

    Multibinding converter for dictionary. Fixes #6
    - Created generic base implementation for multibinding converter that can extract values from an IDictionary
    - Created inherited class that works with IDictionaries with int keys
    - Created inherited class that works with IDictionaries with string keys

---
+++ VaraniumSharp.Shenfield.Tests/Converters/DictionaryValueExtractionConverterTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using FluentAssertions;
+using NUnit.Framework;
+using VaraniumSharp.Shenfield.Converters;
+
+namespace VaraniumSharp.Shenfield.Tests.Converters
+{
+    public class DictionaryValueExtractionConverterTests
+    {
+        #region Public Methods
+
+        [Test]
+        public void ConvertBackThrowANotImplementedException()
+        {
+            // arrange
+            var sut = new DictionaryIntValueExtractionConverter();
+            var act = new Action(() => sut.ConvertBack(new object[0], new Type[0], new object(), CultureInfo.InvariantCulture));
+
+            // act
+            // assert
+            act.ShouldThrow<NotImplementedException>();
+        }
+
+        [Test]
+        public void ValueIsCorrectlyExtractedFromIntKeyedDictionary()
+        {
+            // arrange
+            const int expectedKey = 2;
+            const string expectedValue = "This one";
+            var sut = new DictionaryIntValueExtractionConverter();
+            var dictionary = new Dictionary<int, string>
+            {
+                { 1, "Not this one" },
+                { expectedKey, expectedValue }
+            };
+
+            // act
+            var result = sut.Convert(new object[] { dictionary, expectedKey }, typeof(int), string.Empty, CultureInfo.InvariantCulture);
+
+            // assert
+            result.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void IfThereAreNotEnoughParametersTheBindingDoesNothing()
+        {
+            // arrange
+            var sut = new DictionaryIntValueExtractionConverter();
+
+            // act
+            var result = sut.Convert(new object[] { new Dictionary<int, string>() }, typeof(int), new object(), CultureInfo.InvariantCulture);
+
+            // assert
+            result.Should().Be(Binding.DoNothing);
+        }
+
+        [Test]
+        public void IfCollectionDoesNotImplementIDictionaryTheBindingDoesNothing()
+        {
+            // arrange
+            var sut = new DictionaryIntValueExtractionConverter();
+
+            // act
+            var result = sut.Convert(new object[] { new List<string>(), 1 }, typeof(int), new object(), CultureInfo.InvariantCulture);
+
+            // assert
+            result.Should().Be(Binding.DoNothing);
+        }
+
+        [Test]
+        public void ValueIsCorrectlyExtractedFromStringKeyedDictionary()
+        {
+            // arrange
+            const string expectedKey = "Two";
+            const string expectedValue = "This one";
+            var sut = new DictionaryStringValueExtractionConverter();
+            var dictionary = new Dictionary<string, string>
+            {
+                { "One", "Not this one" },
+                { expectedKey, expectedValue }
+            };
+
+            // act
+            var result = sut.Convert(new object[] { dictionary, expectedKey }, typeof(string), string.Empty, CultureInfo.InvariantCulture);
+
+            // assert
+            result.Should().Be(expectedValue);
+        }
+
+        #endregion
+    }
+}
---
+++ VaraniumSharp.Shenfield/Converters/DictionaryValueExtractionConverterBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace VaraniumSharp.Shenfield.Converters
+{
+    /// <summary>
+    /// Abstract implementation of <see cref="IMultiValueConverter"/> for use in a <see cref="MultiBinding"/> that allows extracting
+    /// values from a Dictionary based on a generic key.
+    /// <remarks>
+    /// The implementation is abstract as there is no easy way in WPF to use generic types, however it is easy and quick to create a strongly typed
+    /// class that inherits the abstract class and use it instead
+    /// </remarks>
+    /// </summary>
+    /// <typeparam name="T">The type value of the dictionary key</typeparam>
+    public abstract class DictionaryValueExtractionConverterBase<T> : IMultiValueConverter
+    {
+        #region Public Methods
+
+        /// <inheritdoc />
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length != 2)
+            {
+                return Binding.DoNothing;
+            }
+
+            var parsedKey = (T)TypeDescriptor
+                .GetConverter(typeof(T))
+                .ConvertFromString(values[1].ToString());
+
+            if (!(values[0] is IDictionary dict) || parsedKey == null)
+            {
+                return Binding.DoNothing;
+            }
+
+            return dict[parsedKey];
+        }
+
+        /// <inheritdoc />
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
